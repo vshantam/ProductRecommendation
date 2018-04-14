017d51db1b48ecef89109154b53c90560f51bc1f
Shantam Vijayputra <vshantam@yahoo.com>
2018-04-15 03:09:28 +0530

    updated the program for python2 and python3

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ Created on Sat Apr 14 18:07:52 2018
 @author: Shantam Vijayputra
 """
 
+#importing the libraries
 import pickle
 import pandas as pd
 import math
@@ -15,22 +16,26 @@ from colorama import init
 from termcolor import cprint
 from pyfiglet import figlet_format
 
-
+#creating class
 class Main(object):
 
+    #initialising path with null
     def __init__(self):
         self.path_with_name = None
 
+    #static method to load classifier
     @staticmethod
     def loadclf(path_with_name):
         _type = 'rb'
         return pickle.load(open(path_with_name, _type))
 
+    #static method to load dataframe
     @staticmethod
     def loaddf(path):
         return pd.read_csv(path, encoding='latin-1', engine='python',
                            dialect=None, sep=',')
-
+    
+    #creating dictionary to map elements in the coloumn
     @classmethod
     def proddict(cls, prod):
         dict = {}
@@ -42,40 +47,52 @@ class Main(object):
 
 if __name__ == '__main__':
 
+    #Ascii art 
     init(strip=not sys.stdout.isatty())
     
     cprint(figlet_format('P R S!', font='starwars'), 'yellow', 'on_red'
            , attrs=['bold'])
     
+    #Abbreviation Printing
     print ('## Product  Recommender     System!##\n')
 
+    #creating he object and instantiate it
     obj = Main()
-
+    
+    #loading the pandas dataframe
     df = obj.loaddf('Book1.csv')
 
+    #printing the instruction to input
     print ("Please provide the product details that you liked in a single line seperated by space in the following order:\n1.Brand\n2.Product\n3.Model\n4.Color\n5.Price\n6.Rating\n")
 
+    #printing the examples
     print ('E.g.')
 
     print (df.head(n=5))
     
+    #displaying the spelling warning
     print ('''
 Please Make sure that  you spell correctly and the product details must be available in our dataset.
 ''')
     
+    #asking for response
     print ('Enter your Response: ')
-
+ 
+    #running through infinite loop until there is 6 inputs in the response. exactly 6 input is must.
     while True:
-
+        
+        #using exceptional handeling to get respone for either python2 and python3 version
         try:
             response = input().strip().split(' ')
         except:
             response = raw_input().strip().split(' ')
-
+        
+        #checking the response is propper or not
         if len(response) != 6:
             print ('Response is not propper')
         else:
             break
+    #exceptional handeling to create dictionary and map using unique numbers
     try:
         try:
 
@@ -109,11 +126,13 @@ Please Make sure that  you spell correctly and the product details must be avail
             print ('Dictionary Failure!')
             print ({}.format(str(e)))
 
+        #variables 
         brand = brand_dict[response[0]]
         product = products_dict[response[1]]
         model = models_dict[response[2]]
         color = colors_dict[response[3]]
 
+        #filter the price because money can be in 13,490 or 13490.we want only float or integer no string or special charecters.
         if ',' in response[4]:
             price = math.ceil(int(response[4].replace(',', '')))
         else:
@@ -121,14 +140,17 @@ Please Make sure that  you spell correctly and the product details must be avail
 
         rating = float(response[5])
 
+        #using exceptional handeling to load trained classifier using python 2 or python3 .
         try:
+	    # .pkl extension for python3
             clf = pickle.load(open('clf.pkl', 'rb'))
 
         except:
-
+            # .sav function for python2
             import cPickle as pickle
-            clf = pickle.load(open('clf.pkl', 'rb'))
-
+            clf = pickle.load(open('clf.sav', 'rb'))
+        
+        #creating index to be fitted in the trained classifier and asking for number of recommendations.
         index = clf.kneighbors([[
             brand,
             product,
@@ -136,18 +158,21 @@ Please Make sure that  you spell correctly and the product details must be avail
             color,
             price,
             rating,
-            ]], int(input('No. of recomendations:').strip()),
+            ]], int(input('No. of recomendations:')),
                 return_distance=False)
 
+        #displaying the order and format for the output
         print ('''
 Brand \t Product \t Model \t Color \tPrice \t Rating
 ''')
+        #displaying  n recomendations
         for i in index[0]:
             print (list(df.iloc[i]))
     except KeyError as  k:
 
         print ({}.format(str(k)))
 
+    #finished.
     print ('Finished procedure')
 
 
--- recom.py
+++ recom.py
@@ -5,41 +5,51 @@ Created on Sat Apr 14 13:24:47 2018
 @author: Shantam Vijayputra
 """
 
+#importing library
 import numpy as np
 import pandas as pd
 from sklearn.neighbors import NearestNeighbors
 import pickle
 
-
+#creating class
 class Predict(object):
 
+    #default path as null
     def __init__(self):
         self.path = None
 
+    #static method to create set of elements in a column
     @staticmethod
     def createset(dataframe, colname):
-        return set(dataframe[colname])
 
+        return set(dataframe[colname])
+    
+    #creating classmethod to load dataset
     @classmethod
     def loaddata(cls, path):
+
         return pd.read_csv(path, sep =",", encoding="latin-1", engine="python", dialect=None, na_filter=True)
 
+    #classmethod to create dictionary
     @classmethod
     def proddict(cls, prod):
         dict = {}
 
         for i in range(len(prod)):
             dict[list(prod)[i]]= i+1
+
         return dict
 
+    #classmethod to format price value into proper integer format
     @classmethod
     def intprice(cls, prices):
-        i: int
         for i in range(prices.shape[0]):
             if "," in str(prices[i]):
                 prices[i] = int(prices[i].replace(",", ""))
+
         return prices
 
+    #classmethod to save trained model
     @classmethod
     def save_clf(self, clf, path_with_name, _type):
 
@@ -50,10 +60,13 @@ class Predict(object):
 
 if __name__=="__main__":
 
+    #reating the object
     obj = Predict()
-
+    
+    #loading the pandas dataframe
     df = obj.loaddata("Book1.csv")
-
+    
+    #creating sets of each column for unique elements
     brands = obj.createset(df,'Brand')
     products = obj.createset(df,'Product')
     models = obj.createset(df,'model')
@@ -61,17 +74,20 @@ if __name__=="__main__":
     prices = df['Price']
     ratings =  df['Rating']
 
-
+    #creating column wise dictionary to map each value
     brand_dict = obj.proddict(brands)
     products_dict = obj.proddict(products)
     models_dict = obj.proddict(models)
     colors_dict = obj.proddict(colors)
     prices = obj.intprice(prices)
-
+    
+    #list of dictionary
     dictlist = [brand_dict, products_dict, models_dict, colors_dict, prices, ratings]
-
+  
+    #new matrix in which algorithm is going to perform
     new_data_frame = np.zeros(df.shape)
 
+    #mapping the values and copying to new matrix
     for i in range(df.shape[0]):
         for j in range(df.shape[1]):
             if j <=3:
@@ -79,15 +95,19 @@ if __name__=="__main__":
             else:
                 new_data_frame[i][j] =  df.iloc[i][j]
 
+    #list of algorithm that can be used
     algorithms = ["auto", "ball_tree", "kd_tree", "brute"]
-
+    
+    #testing with each algorithm
     for i in algorithms:
 
         clf = NearestNeighbors(n_neighbors=5, algorithm=algorithms[0]).fit(new_data_frame)
         distances, indices = clf.kneighbors(new_data_frame)
-
-    path_with_name = "clf.pkl"
+ 
+   #saving the trained classifier
+    path_with_name = ["clf.pkl", "clf.sav"]
     _type = "wb"
-    obj.save_clf(clf, path_with_name, _type)
+    obj.save_clf(clf, path_with_name[0], _type) #for python2
+    obj.save_clf(clf, path_with_name[1], _type) #for python3
 
-    
+###finished ###
