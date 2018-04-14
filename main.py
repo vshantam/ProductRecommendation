84c3f3a7cc148ad50b49537f69a5b87269ff6bf7
Shantam Vijayputra <vshantam@yahoo.com>
2018-04-14 21:19:00 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 14 18:07:52 2018
+
+@author: Shantam Vijayputra
+"""
+import pickle
+import pandas as pd
+import math
+
+class Main(object):
+    
+    def __init__(self):
+        self.path_with_name = None
+        
+    @staticmethod
+    def loadclf(path_with_name):
+        _type = "rb"
+        return pickle.load(open(path_with_name,_type))
+    
+    @staticmethod
+    def loaddf(path):
+        return pd.read_csv(path, encoding="latin-1", engine="python", dialect=None, sep=",")
+
+    @classmethod
+    def proddict(cls, prod):
+        dict = {}
+
+        for i in range(len(prod)):
+            dict[list(prod)[i]]= i+1
+        return dict
+
+     
+        
+if __name__ == "__main__":
+    
+    obj = Main()
+    
+    df = obj.loaddf("Book1.csv")
+    print("Please provide the product details that you liked in a single line seperated by space in the following order i:\n1.Brand\n2.Product\n3.Model\n4.Color\n5.Price\n6.Rating\n");
+    print("E.g.")
+    print(df.head(n = 5))
+    print("Enter your Response: ")
+
+    while(True):
+        
+        response = input().strip().split(" ")
+    
+        if len(response) != 6:
+            print({}.format("Response is not propper"))
+        
+        else:
+            break
+        
+        
+    try:
+        try:
+            
+            brand_dict = {'Brand1': 1, 'Brand2': 3, 'Brand3': 4, 'Brand4': 2, 'Brand5': 5}
+            colors_dict = {'Black': 4, 'Grey': 3, 'PInk': 1, 'Silver': 5, 'White': 2}
+            products_dict = {'Camera': 2, 'Lens': 3, 'Mobile': 4, 'T.V': 1}
+            models_dict = obj.proddict(set(df['model']))
+            
+        except Exception as e:
+            print("Dictionary Failure!")
+            print({}.format(str(e)))
+        
+        brand = brand_dict[response[0]];
+        product = products_dict[response[1]];
+        model = models_dict[response[2]];
+        color = colors_dict[response[3]];
+        
+        if "," in response[4]:
+            price = math.ceil(int((response[4].replace("," , ""))))
+        else:
+            price = math.ceil(int((response[4])))
+            
+        rating = float(response[5])
+        
+        clf = pickle.load(open("clf.pkl","rb"))
+        
+        index = clf.kneighbors([[brand, product, model, color, price, rating]], int(input("No. of recomendations:").strip()), return_distance=False)
+        
+        for i in index[0]:
+            print(list(df.iloc[i]))
+            
+    except KeyError as k:
+        print({}.format(str(k)))
+        
+    print("Finished procedure")
+
+            
+        
+            
---
+++ recom.py
@@ -0,0 +1,93 @@
+# -*- Dataset coding: Latin-1 -*-
+"""
+Created on Sat Apr 14 13:24:47 2018
+
+@author: Shantam Vijayputra
+"""
+
+import numpy as np
+import pandas as pd
+from sklearn.neighbors import NearestNeighbors
+import pickle
+
+
+class Predict(object):
+
+    def __init__(self):
+        self.path = None
+
+    @staticmethod
+    def createset(dataframe, colname):
+        return set(dataframe[colname])
+
+    @classmethod
+    def loaddata(cls, path):
+        return pd.read_csv(path, sep =",", encoding="latin-1", engine="python", dialect=None, na_filter=True)
+
+    @classmethod
+    def proddict(cls, prod):
+        dict = {}
+
+        for i in range(len(prod)):
+            dict[list(prod)[i]]= i+1
+        return dict
+
+    @classmethod
+    def intprice(cls, prices):
+        i: int
+        for i in range(prices.shape[0]):
+            if "," in str(prices[i]):
+                prices[i] = int(prices[i].replace(",", ""))
+        return prices
+
+    @classmethod
+    def save_clf(self, clf, path_with_name, _type):
+
+        # dumping the data
+        return pickle.dump(clf, open(path_with_name, _type))
+
+
+
+if __name__=="__main__":
+
+    obj = Predict()
+
+    df = obj.loaddata("Book1.csv")
+
+    brands = obj.createset(df,'Brand')
+    products = obj.createset(df,'Product')
+    models = obj.createset(df,'model')
+    colors = obj.createset(df,'Color')
+    prices = df['Price']
+    ratings =  df['Rating']
+
+
+    brand_dict = obj.proddict(brands)
+    products_dict = obj.proddict(products)
+    models_dict = obj.proddict(models)
+    colors_dict = obj.proddict(colors)
+    prices = obj.intprice(prices)
+
+    dictlist = [brand_dict, products_dict, models_dict, colors_dict, prices, ratings]
+
+    new_data_frame = np.zeros(df.shape)
+
+    for i in range(df.shape[0]):
+        for j in range(df.shape[1]):
+            if j <=3:
+                new_data_frame[i][j] = dictlist[j][df.iloc[i][j]]
+            else:
+                new_data_frame[i][j] =  df.iloc[i][j]
+
+    algorithms = ["auto", "ball_tree", "kd_tree", "brute"]
+
+    for i in algorithms:
+
+        clf = NearestNeighbors(n_neighbors=5, algorithm=algorithms[0]).fit(new_data_frame)
+        distances, indices = clf.kneighbors(new_data_frame)
+
+    path_with_name = "C:/Users/Shantam Vijayputra/Desktop/intern/clf.pkl"
+    _type = "wb"
+    obj.save_clf(clf, path_with_name, _type)
+
+    
